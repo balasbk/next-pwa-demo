953572bba612516356cc96ff63e6f1282e5c340f
Balakumar <balasbk1999@gmail.com>
2024-06-27 00:30:22 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+import withPWAInit from "next-pwa";
+
 const withNextIntl = createNextIntlPlugin('./i18n.js');
+
+
+
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
  
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    
-};
+const nextConfig = {};
  
-export default withNextIntl(nextConfig);
+export default withPWA(withNextIntl(nextConfig));
